e572bf84087b79df02af40ee49a4d5bd5efe5aa1
KaeOkamura <kae.okamura@partridgesystems.com>
2025-02-10 08:16:37 +0900

    refactor: clean up code structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,24 @@ import { FallingModel } from "./components/FallingModel";
 import { Fog } from "./components/Fog";
 import "./styles.css";
 
-const modelCount = 150;
+const MODEL_COUNT = 150;
+const MIN_SPEED = 0.02;
+const MAX_SPEED = 0.05;
+const MIN_DELAY = 0;
+const MAX_DELAY = 2;
+const INITIAL_Y_RANGE = [10, 20];
+const POSITION_RANGE = [-10, 10];
 
-export default function App() {
-  const getRandomSpeed = () => {
-    const minSpeed = 0.02;
-    const maxSpeed = 0.05;
-    return Math.random() * (maxSpeed - minSpeed) + minSpeed;
-  };
+const getRandomValue = (min, max) => Math.random() * (max - min) + min;
 
-  const getRandomDelay = () => {
-    const minDelay = 0;
-    const maxDelay = 2;
-    return Math.random() * (maxDelay - minDelay) + minDelay;
-  };
-
-  const models = Array.from({ length: modelCount }).map((_, index) => {
-    const initialY = Math.random() * 10 + 10;
-    const speed = getRandomSpeed();
-    const delay = getRandomDelay();
-    const xPosition = (index / modelCount) * 20 - 10;
+const createModels = () => {
+  return Array.from({ length: MODEL_COUNT }).map((_, index) => {
+    const initialY = getRandomValue(...INITIAL_Y_RANGE);
+    const speed = getRandomValue(MIN_SPEED, MAX_SPEED);
+    const delay = getRandomValue(MIN_DELAY, MAX_DELAY);
+    const xPosition =
+      (index / MODEL_COUNT) * (POSITION_RANGE[1] - POSITION_RANGE[0]) +
+      POSITION_RANGE[0];
 
     return {
       position: [xPosition, initialY, (Math.random() - 0.5) * 10],
@@ -32,13 +30,19 @@ export default function App() {
       delay,
     };
   });
+};
+
+export default function App() {
+  const models = createModels();
 
   return (
     <Canvas camera={{ fov: 45, position: [0, 1, 10], near: 1, far: 100 }}>
       <ambientLight intensity={1} />
       <directionalLight position={[5, 10, 5]} intensity={1} />
+
       <Fog />
       <Environment preset="warehouse" />
+
       {models.map(({ position, speed, delay }, i) => (
         <FallingModel
           key={i}
@@ -47,6 +51,7 @@ export default function App() {
           delay={delay}
         />
       ))}
+
       <OrbitControls minDistance={6} maxDistance={15} />
     </Canvas>
   );
